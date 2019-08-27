425e05cf7e414bd7e7a7be0fa5f866f5d120af30
Gustavo Rocha Tambelini <rochatambelini@gmail.com>
2019-08-27 04:39:28 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/rochatambelini/ProjetojpaApplication.java
+++ src/main/java/com/rochatambelini/ProjetojpaApplication.java
@@ -64,6 +64,10 @@ public class ProjetojpaApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "RH");
+		Categoria cat4 = new Categoria(null, "Tesouraria");
+		Categoria cat5 = new Categoria(null, "Produção");
+		Categoria cat6 = new Categoria(null, "Maquinistas");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -76,7 +80,7 @@ public class ProjetojpaApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/rochatambelini/services/CategoriaService.java
+++ src/main/java/com/rochatambelini/services/CategoriaService.java
@@ -1,12 +1,17 @@
 package com.rochatambelini.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
 import com.rochatambelini.domain.Categoria;
+import com.rochatambelini.dto.CategoriaDTO;
 import com.rochatambelini.repositores.CategoriaRepository;
 import com.rochatambelini.services.exceptions.DateIntegrityExeption;
 import com.rochatambelini.services.exceptions.ObjectNotFoundException;
@@ -46,5 +51,14 @@ public class CategoriaService {
 	public List <Categoria> findAll() {
 		List <Categoria> list = repo.findAll();
 		return list;
+	}
+	
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
