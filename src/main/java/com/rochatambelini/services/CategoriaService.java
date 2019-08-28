34016fcdaa800737598464e2caa0f93936603963
Gustavo Rocha Tambelini <rochatambelini@gmail.com>
2019-08-28 04:50:57 -0300

    Correções: udate em CategoriaService e construtor de Pagamento

--- src/main/java/com/rochatambelini/services/CategoriaService.java
+++ src/main/java/com/rochatambelini/services/CategoriaService.java
@@ -34,7 +34,8 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		buscar(obj.getId());
+		Categoria newObj = buscar(obj.getId());
+		updateData(newObj,obj);
 		return repo.save(obj); 
 	}
 
@@ -60,5 +61,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	public void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
